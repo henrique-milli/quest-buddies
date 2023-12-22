4834179ed5d87c256350fe053d1f59b1aaecae2c
henriquemilli <info@henriquemilli.com>
2023-12-23 00:14:02 +0100

    non static

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: "export",
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
